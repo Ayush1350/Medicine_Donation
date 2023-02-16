65a30c1b7830a611807a05421274e81ead70bdbb
Dell <ayushpatel20@gnu.ac.in>
2023-02-16 22:21:08 +0530

    Phase-14

--- app/src/main/java/com/example/MedicineDonationApp/HomeActivity.kt
+++ app/src/main/java/com/example/MedicineDonationApp/HomeActivity.kt
@@ -5,57 +5,47 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
-import com.example.MedicineDonationApp.databinding.ActivityHistoryBinding
 import com.example.MedicineDonationApp.databinding.ActivityHomeBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
-    private lateinit var bottomNavigation: BottomNavigationView
     lateinit var viewPager: ViewPager
     lateinit var viewPagerAdapter: ViewPagerAdapter
     lateinit var imageList: List<Int>
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//...............................................IMAGE SLIDER........................................................//
+//......................................................IMAGE SLIDER.......................................................//
 
         viewPager = findViewById(R.id.idViewPager)
         imageList = ArrayList<Int>()
         imageList = imageList + R.drawable.img
         imageList = imageList + R.drawable.img_1
-        imageList = imageList + R.drawable.img_2
-        imageList = imageList + R.drawable.img_3
         viewPagerAdapter = ViewPagerAdapter(this@HomeActivity, imageList)
         viewPager.adapter = viewPagerAdapter
 
-//...............................................UPLOAD ACTIVITY........................................................//
+//......................................................UPLOAD ACTIVITY.......................................................//
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         binding.Donate.setOnClickListener {
             val intent = Intent (this,UploadActivity::class.java)
             startActivity(intent)
         }
 
-//...............................................HISTORY ACTIVITY........................................................//
+//......................................................HISTORY ACTIVITY.......................................................//
 
-        binding.bottomNavigationView.selectedItemId = R.id.home
-        binding.bottomNavigationView.selectedItemId = R.id.location
-        binding.bottomNavigationView.selectedItemId = R.id.person
-        binding.bottomNavigationView.setOnItemSelectedListener { it ->
+        binding.bottomNavigationView.selectedItemId = R.id.history
+        binding.bottomNavigationView.setOnItemSelectedListener {it
             when (it.itemId) {
                 R.id.history -> {
-                    Intent(this, HistoryActivity::class.java).also {
-                        startActivity(it) }
+                    Intent(this, HistoryActivity::class.java).also{ startActivity(it) }
                 }
             }
             return@setOnItemSelectedListener true
         }
-
-
-//...............................................LOCATION ACTIVITY........................................................//
+//......................................................LOCATION ACTIVITY.......................................................//
 
         binding.bottomNavigationView.selectedItemId = R.id.location
         binding.bottomNavigationView.setOnItemSelectedListener {it
