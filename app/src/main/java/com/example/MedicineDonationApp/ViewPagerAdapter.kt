8afb9bd3ca367fd7192abd8a2e40035c37dd14ea
Dell <ayushpatel20@gnu.ac.in>
2023-02-16 21:27:32 +0530

    Phase-12

---
+++ app/src/main/java/com/example/MedicineDonationApp/ViewPagerAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.MedicineDonationApp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.viewpager.widget.PagerAdapter
+import java.util.*
+
+class ViewPagerAdapter(val context: Context, val imageList: List<Int>) : PagerAdapter() {
+    override fun getCount(): Int {
+        return imageList.size
+    }
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as RelativeLayout
+    }
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val mLayoutInflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val itemView: View = mLayoutInflater.inflate(R.layout.image_slider_item, container, false)
+        val imageView: ImageView = itemView.findViewById<View>(R.id.idIVImage) as ImageView
+        imageView.setImageResource(imageList.get(position))
+        Objects.requireNonNull(container).addView(itemView)
+        return itemView
+    }
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+
+        container.removeView(`object` as RelativeLayout)
+    }
+}
