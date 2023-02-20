f63a3261dde7e46f113a8210697f5781d6beb9b8
Dell <ayushpatel20@gnu.ac.in>
2023-02-20 22:45:41 +0530

    Phase-19

--- app/src/main/java/com/example/MedicineDonationApp/ProfileFragment.kt
+++ app/src/main/java/com/example/MedicineDonationApp/ProfileFragment.kt
@@ -1,10 +1,19 @@
 package com.example.MedicineDonationApp
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.MedicineDonationApp.databinding.ActivityItemImageBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,9 +26,40 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ProfileFragment
+    private lateinit var profileImage: ImageView
+    private lateinit var profilePicture: Uri
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Initialize global variables
+        profileImage = view.findViewById(R.id.profileImage)
+
+        //Handle profile image upload
+        profileImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            profilePicture = data.data!!
+            val bitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, profilePicture)
+            profileImage.setImageBitmap(bitmap)
+        }
+    }
+
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +75,7 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
     }
 
     companion object {
